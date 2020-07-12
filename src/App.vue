5e43462a67b17fea6b4e6e6761787e06402bd28c
M-HARD <mumusab268000@gmail.com>
2020-07-12 05:45:53 +0200

    add main foldeer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <div class="flex justify-center test"><img alt="Vue logo" src="./assets/img/logo.png"></div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="l16 bg-gray-100 min-h-screen" dir="rtl">
+    <div class="p-5 mx-10">
+      <nav class="w-full py-2 flex justify-center bg-gray-600">
+        <span @click="navigate('app-main-ui')" class="mx-3 px-5 py-2 text-xl cursor-pointer bg-gray-400 rounded">main</span>
+        <span @click="navigate('app-user-config-ui')" class="mx-3 px-5 py-2 text-xl cursor-pointer bg-gray-400 rounded">UserConfigUI</span>
+        <span @click="navigate('app-view-some-data')" class="mx-3 px-5 py-2 text-xl cursor-pointer bg-gray-400 rounded">some data</span>
+      </nav>
+      
+      <keep-alive>
+        <component :is="currentView" :data="{subInClasses:subInClasses,userConfig:userConfig}"></component>
+      </keep-alive>
+
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { eventBus } from '@/main'
+import data from './data/data'
+import MainUI from './MainUI'
+import UserConfigUI from './UserConfigUI'
+import viewSomeData from './ViewSomeData'
+import Vue from 'vue'
 
+
+Vue.component('app-main-ui',MainUI)
+Vue.component('app-user-config-ui',UserConfigUI)
+Vue.component('app-view-some-data',viewSomeData)
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'L17_f',
+  mixins:[data],
+  data() {
+    return {
+      currentView:'app-main-ui',
+      subInClasses:[],
+      userConfig:[],
+    }
+  },
+  created() {
+    this.addSubInClass()
+    eventBus.$on('retailUserConfig',(data)=>{
+      this.userConfig = data
+    })
+  },
+  methods: {
+    navigate(nextUrl){
+      this.currentView = nextUrl
+    },
+    addSubInClass(){
+            var id=0
+            for (let y = 0; y < this.classes.length; y++) {
+              var theClass = this.classes[y];
+              var subInClass = []
+              
+              for (let x = 0; x < this.dayOfWeek.length; x++) {
+                var sub = []
+                for (let i = 0; i < theClass.subInDay; i++) {
+                  sub.push({
+                      id:id,
+                      sId:i+','+x+','+y,
+                      oId:[y,x,i],
+                      subject:{id:null,name:null},
+                      teacher:{id:null,name:null}
+                    })
+                  id++;
+                }
+                subInClass.push(sub)
+              }
+              this.subInClasses.push(subInClass)
+            }
+          },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
