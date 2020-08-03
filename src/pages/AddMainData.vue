f8ead5877642626107f486d8d4a4c74d346e8614
M-HARD <mumusab268000@gmail.com>
2020-08-03 21:36:51 +0200

    can add teatcher

--- src/pages/AddMainData.vue
+++ src/pages/AddMainData.vue
@@ -87,6 +87,34 @@
           </table>
         </div>
 
+        <div class="flex justify-center py-10">
+          <table class="table-auto mt-10 rounded shadow-xl overflow-hidden">
+            <thead>
+              <tr class="flex flex-wrap font-bold border-b-2 border-gray-400 items-center bg-gray-400">
+                <td class="w-20 font-extrabold text-xl"></td>
+                <td class="w-32 font-extrabold text-xl">عدد ايام الأسبوع</td>
+                <td class="w-32 font-extrabold text-xl text-center">حذف</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="flex flex-wrap border-b-2 border-gray-300 bg-white hover:bg-gray-300" v-for="(day,i) in data.mainData.teachers" :key="i">
+                <td class="w-20 font-semibold px-2">{{ i+1 }}</td>
+                <td class="w-32 font-semibold">{{ day.name }}</td>
+                <td class="w-32 font-semibold text-center">
+                  <span @click="data.mainData.teachers.splice(i,1)" class="text-red-500 cursor-pointer">X</span>
+                </td>
+              </tr>
+              <tr class="flex flex-wrap border-b-2 border-gray-300 bg-white">
+                <td class="w-20 font-semibold px-2"></td>
+                <td class="w-32 font-semibold"><input type="text" v-model="userInput.teacherInput" class="w-full h-full bg-gray-400 p-1"></td>
+                <td class="w-32 font-semibold text-center flex items-center">
+                  <span @click="AddTeacher" class="bg-green-400 hover:bg-green-500 w-full py-1 rounded-md cursor-pointer">أضافة</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
       </div>
 
 
@@ -110,7 +138,8 @@ export default {
       userInput:{
           dayInput:null,
           classInput:null,
-          subjectInput:null
+          subjectInput:null,
+          teacherInput:null
       },
     }
   },
@@ -147,6 +176,17 @@ export default {
               name:this.userInput.subjectInput
         })
         this.userInput.subjectInput = null
+      },
+      AddTeacher(){
+        if(this.userInput.teacherInput == null)return
+        let lastId = Math.max(...this.data.mainData.teachers.map(day=>{
+            return day.id
+        }))
+        this.data.mainData.teachers.push({
+              id:lastId+1,
+              name:this.userInput.teacherInput
+        })
+        this.userInput.teacherInput = null
       }
   },
 }
